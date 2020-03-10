c3746532c5f34d22f2d15ff7a38bd266cc366811
Rob Terrell <Rterrell25@gmail.com>
2020-03-10 10:18:05 -0400

    added cleanup to the useEffect to ensure location tracking is not continuously running in the background causing memory leaks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { useState, useEffect } from "react"
 
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+}
+
 const App = () => {
   const [count, setCount] = useState(0)
   const [isOn, setIsOn] = useState(false)
   const [mousePosition, setMousePosition] = useState({ x: null, y: null })
+  const [status, setStatus] = useState(navigator.onLine)
+  const [{ latitude, longitude, speed }, setLocation] = useState(
+    initialLocationState
+  )
 
   useEffect(() => {
     document.title = `You have clicked ${count} times`
     window.addEventListener("mousemove", handleMouseMove)
-
+    window.addEventListener("onine", handleOnline)
+    window.addEventListener("offline", handleOffline)
+    navigator.geolocation.getCurrentPosition(handleGeoLocation)
+    const watchId = navigator.geolocation.watchPosition(handleGeoLocation)
     return () => {
       window.removeEventListener("mousemove", handleMouseMove)
+      window.removeEventListener("onine", handleOnline)
+      window.removeEventListener("offline", handleOffline)
+      navigator.geolocation.clearWatch(watchId)
     }
   }, [count])
+
+  const handleOnline = () => {
+    setStatus(true)
+  }
+
+  const handleOffline = () => {
+    setStatus(false)
+  }
 
   const handleMouseMove = event => {
     setMousePosition({
@@ -27,6 +51,14 @@ const App = () => {
 
   const toggleLight = () => {
     setIsOn(previsOn => !previsOn)
+  }
+
+  const handleGeoLocation = event => {
+    setLocation({
+      latitude: event.coords.latitude,
+      longitude: event.coords.longitude,
+      speed: event.coords.speed
+    })
   }
 
   return (
@@ -50,6 +82,14 @@ const App = () => {
       <h2>Mouse Position</h2>
       {JSON.stringify(mousePosition, null, 2)}
       <br />
+      <h2>Network Status</h2>
+      <p>
+        you are <strong>{status ? "online" : "offline"}</strong>
+      </p>
+      <h2>GeoLocation</h2>
+      <p>Latitude is {latitude}</p>
+      <p>longitude is {longitude}</p>
+      <p>Your speed is {speed ? speed : "0"}</p>
     </>
   )
 }
