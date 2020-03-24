4491b7185782e89f2f00bcb7e385676bb98b951f
Rob Terrell <Rterrell25@gmail.com>
2020-03-24 17:39:14 -0400

    added an if statement to useEffect to prevent it from running unless the submit state is true

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,49 @@
-import React, { useState, useEffect, useCallback } from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios"
 
 const Home = () => {
+  const [submitting, setSubmitting] = useState(true)
   const [data, setData] = useState(null)
   const [query, setQuery] = useState("react hooks")
 
-  const getData = useCallback(async () => {
-    const response = await axios.get(
-      `http://hn.algolia.com/api/v1/search?query=${query}`
-    )
-    setData(response.data)
-  }, [query])
-
   useEffect(() => {
-    getData()
-  }, [getData])
+    // query can change, but don't actually trigger request unless submitting is true
+    const getData = async () => {
+      const response = await axios.get(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+      )
+      setData(response.data)
+      console.log(response.data)
+      setSubmitting(false) // call is finished, set to false
+    }
+
+    // query can change, but don't actually trigger
+    // request unless submitting is true
+
+    if (submitting) {
+      // is true initially, and again when button is clicked
+      getData()
+    }
+  }, [submitting, query])
 
   const handleChange = event => {
     event.preventDefault()
     setQuery(event.target.value)
   }
 
+  const getData = () => setSubmitting(true)
+
+  const handleSearch = event => {
+    event.preventDefault()
+    getData()
+  }
+
   return (
     <div>
-      <input type='text' onChange={handleChange} value={query} />
-      <button type='button' onClick={getData}>
-        Submit
-      </button>
+      <form onSubmit={handleSearch}>
+        <input type='text' onChange={handleChange} value={query} />
+        <button type='submit'>Submit</button>
+      </form>
       {data &&
         data.hits.map(item => (
           <div key={item.objectID}>
